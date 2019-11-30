34d1f6e9926578ac7568320b4579e4231f782b73
akshat <akshatsharma46@gmail.com>
2019-11-30 10:31:49 -0800

    cuid.c

--- arch/x86/kvm/cpuid.c
+++ arch/x86/kvm/cpuid.c
@@ -999,8 +999,8 @@ int kvm_emulate_cpuid(struct kvm_vcpu *vcpu)
 		case 0x4FFFFFFD:
 			printk("ecx: %lu\n", (unsigned long)ecx);
 			temp = eax;
-			eax = atomic_read(&vmexit_info_array[temp]->count);
-			printk("Total time spent in vmx: %lu\n", atomic_read(&vmexit_info_array[temp].count));
+			eax = atomic_read(&vmexit_info_array[temp].count);
+			printk("Total time spent in vmx: %lu\n", eax);
 			break;
 		case 0x4FFFFFFC:
 			edx = 4567;
